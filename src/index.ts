e62ea06dffe236f98c86455de958928641d36d45
a.alekhin <a.alekhin@megapolis-it.ru>
2026-06-09 19:03:28 +0300

    feat: di

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Happy developing ✨')
+export { Container, Optional } from "./container";
+export { ContainerInstance } from "./container-instance";
+export { CyclicDependencyError, ServiceNotFoundError } from "./errors";
+export { Token } from "./token";
+export type {
+    AnyDependency,
+    Constructor,
+    Disposable,
+    Scope,
+    ServiceId,
+} from "./types";
